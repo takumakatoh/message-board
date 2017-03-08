5bdc4624ee37748c3a3ebf38e6372cc5b5d4eec0
takumakatou <amagami0086@gmail.com>
2017-03-08 13:03:37 +0000

    kadai3

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,44 +1,44 @@
 class MessagesController < ApplicationController
-before_action :set_message, only: [:edit,:update,:destroy]
+  before_action :set_message, only: [:edit,:update,:destroy]
 
-def index
-@message = Message.new
-@messages = Message.all
-end
+  def index
+    @message = Message.new
+    @messages = Message.all
+  end
 
-def create
-@message = Message.new(message_params)
-if @message.save
-redirect_to root_path , notice: 'メッセージを保存しました'
-else
-@messages = Message.all
-flash.now[:alert] = "メッセージの保存に失敗しました。"
-render 'index'
-end
-end
+  def create
+    @message = Message.new(message_params)
+    if @message.save
+      redirect_to root_path , notice: 'メッセージを保存しました'
+    else
+      @messages = Message.all
+      flash.now[:alert] = "メッセージの保存に失敗しました。"
+      render 'index'
+    end
+  end
 
-def edit
-end
+  def edit
+  end
 
-def update
-if @message.update(message_params)
-redirect_to root_path , notice: 'メッセージを編集しました'
-else
-render 'edit'
-end
-end
+  def update
+    if @message.update(message_params)
+      redirect_to root_path , notice: 'メッセージを編集しました'
+    else
+      render 'edit'
+    end
+  end
 
-def destroy
-@message.destroy
-redirect_to root_path, notice: 'メッセージを削除しました'
-end
+  def destroy
+    @message.destroy
+    redirect_to root_path, notice: 'メッセージを削除しました'
+  end
 
 private
-def message_params
-params.require(:message).permit(:name, :body)
-end
+  def message_params
+    params.require(:message).permit(:name, :body,:age)
+  end
 
-def set_message
-@message = Message.find(params[:id])
-end
+  def set_message
+    @message = Message.find(params[:id])
+  end
 end
